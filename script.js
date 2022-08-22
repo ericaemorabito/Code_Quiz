6f29f43284dc721380422033d441f3f553cbc171
EricaE <106550558+ericaemorabito@users.noreply.github.com>
2022-08-22 09:53:06 -0500

    Correct answer btn Q1 working

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ var saveButton = document.getElementById("start_button");
 var timeRemaining = document.getElementById("seconds_remaining");
 //Correct answer button
 var correctAnswer = document.querySelector(".correct_answer");
+// Wrong answer buttons
+var wrongAnswer = document.querySelector(".wrong_answer");
 //Correct and wrong message
 var correctOrWrongMessage = document.querySelector(".correct_wrong_message");
 //play Again button
@@ -16,32 +18,101 @@ var playAgain = document.getElementById("play_again_button");
 var finalScore = document.getElementById("final_score");
 
 //Each Page
-//Welcome Page
 var welcomePage = document.getElementById("welcome_message");
-//Question 1
 var questionOnePage = document.getElementById("question_1");
-//Question 2
 var questionTwoPage = document.getElementById("question_2");
-//Question 3
 var questionThreePage = document.getElementById("question_3");
-//Game Over Page
 var gameOverPage = document.getElementById("game_over");
-//High Score Page
 var highScoresPage = document.getElementById("high_score_page");
 
-//Game starts with 60 seconds
-var time = 60;
+//Functions for showing and hiding each page
+var hideWelcome = function() {
+  welcomePage.setAttribute("data-state", "hidden");
+}
+
+var showQ1 = function() {
+  questionOnePage.setAttribute("data-state", "visible");
+}
+
+var hideQ1 = function() {
+  questionOnePage.setAttribute("data-state", "hidden");
+}
+
+var showQ2 = function() {
+  questionTwoPage.setAttribute("data-state", "visible");
+}
+
+var hideQ2 = function() {
+  questionTwoPage.setAttribute("date-state", "hidden")
+}
+
+var showQ3 = function() {
+  questionThreePage.setAttribute("data-state", "visible");
+}
+
+var hideQ3 = function() {
+  questionThreePage.setAttribute("date-state", "hidden")
+}
+
+var showGameOver = function() {
+  gameOverPage.setAttribute("data-state", "visible");
+}
+
+var showHighScores = function() {
+  highScoresPage.setAttribute("data-state", "visible");
+}
+
+//Function for displaying wrong message
+var displayWrongMessage = function(){
+  correctOrWrongMessage.innerHTML = "Wrong";
+  correctOrWrongMessage.setAttribute("style", "red");
+  
+}
+
+//TODO: Correct answer is clicked --DIDN'T WORK WITHIN FUNCTION
+var correctAnswerClicked = function() {
+  correctAnswer.addEventListener("click");
+}
+
+//TODO: Wrong answer is clicked --DIDN'T WORK IN FUNCTION
+var wrongAnswerClicked = function(){
+  wrongAnswer.addEventListener("click");
+}
+
+//Game starts with 30 seconds
+var time = 30;
 
 //Starting Quiz when button clicked
 function startQuiz() {
   startButton.addEventListener('click', function() {
     startTimer();
     // Hides the welcome page
-    welcomePage.setAttribute("data-state", "hidden");
+    hideWelcome();
     // Shows question 1
-    questionOnePage.setAttribute("data-state", "visible");
+    showQ1(); })
+  
+  //correct answer button clicked --WORKS
+  correctAnswer.addEventListener('click', function() {
+    hideQ1();
+    showQ2();})
+  
+    //TODO:wrong answer button clicked
+  wrongAnswer.addEventListener('click', function() {
+    hideQ1();
+    showQ2();
+    displayWrongMessage();
   })
-  };
+
+  //TRYING - make everything into their own function to not repeat
+  // if (correctAnswerClicked) {
+  //   hideQ1();
+  //   showQ2();
+  // } else if (wrongAnswerClicked) {
+  //   hideQ1();
+  //   showQ2();
+  //   displayWrongMessage(); }
+ };
+
 
 startQuiz();
 
@@ -59,8 +130,6 @@ function startTimer() {
   }, 1000);
 }
 
-//Function Questions Begins
-
-//JavaScript to hide/reveal things
-//element.style.display = "none";
-//element.style.display = "block";
+//TODO: Timer loses 10 seconds
+//TODO: End Game
+//TODO: Show highscores page
